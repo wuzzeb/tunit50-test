bb24539353255981513cdd34cbdfc06745d2c055
John Lenz <wuzzeb@gmail.com>
2025-08-04 11:31:28 -0500

    initial test

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TUnit;
+
+public class Test1
+{
+  [Test]
+  public void TestMethod1()
+  {
+    if (1 + 1 != 2)
+    {
+      throw new Exception("1 + 1 should equal 2");
+    }
+  }
+}
